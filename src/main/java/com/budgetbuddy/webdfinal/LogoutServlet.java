4c48681e145f0a26e5f84abeaed9e78b3607d367
Mark Deyarmond <deyarmondmark@gmail.com>
2024-04-04 16:50:14 -0300

    Created servlets

--- src/main/java/com/budgetbuddy/webdfinal/LogoutServlet.java
+++ src/main/java/com/budgetbuddy/webdfinal/LogoutServlet.java
@@ -1,4 +1,24 @@
 package com.budgetbuddy.webdfinal;
 
-public class LogoutServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        response.sendRedirect("index.jsp");
+    }
 }
